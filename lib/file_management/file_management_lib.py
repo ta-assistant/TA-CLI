6989a9ca42758186acb2648df46af9aa4cf5d38d
Vitvara Varavithya <gridxlgm@gmail.com>
2021-05-15 16:03:35 +0700

    file manage lib New DirManagement

--- lib/file_management/file_management_lib.py
+++ lib/file_management/file_management_lib.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 class FileEditor:
     @staticmethod
@@ -12,3 +13,22 @@ class FileEditor:
         else:
             print("The "+path+filename+" does not exist")
         
+class DirManagement:
+    @staticmethod
+    def create_dir(path):
+        try:
+            os.mkdir(path)
+        except OSError:
+            print ("Creation of the directory %s failed" % path)
+        else:
+            print ("Successfully created the directory %s " % path)
+        
+    @staticmethod
+    def remove_dir(path):
+        try:
+            shutil.rmtree(path)
+        except OSError:
+            print ("Deletion of the directory %s failed" % path)
+        else:
+            print ("Successfully deleted the directory %s" % path)
+                
