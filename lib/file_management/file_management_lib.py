5ad9cda053ba8932fa369c2aa260512c53445166
Vitvara Varavithya <gridxlgm@gmail.com>
2021-05-15 18:12:08 +0700

    add docstring file management lib

--- lib/file_management/file_management_lib.py
+++ lib/file_management/file_management_lib.py
@@ -1,3 +1,37 @@
+"""
+Author Vitvara
+
+# This libary is about manage file and directory you can import those module to use by
+
+from file_management_lib import module name
+
+# if you want to use only 1 method you can do it by
+
+method = Object().method
+method(parameter)
+
+# or if you use more than 1 method you should create an variable to keep those Object
+
+object1 = Object1()
+
+object1.method1()
+object1.method2(parameter)
+
+# to be clear about those parameter
+# `path` on window path can't directly use by them self you need to add r in font of the path string and double backslash in the end of path string
+r"this\is\path\\" 
+# in Dirmanagement you should put directory name that you want in to the back of the path
+r"this\is\path\dirname" 
+
+### anyone whio uses macOS, please give me some information that what is the format of the path ###
+
+# `filename` need to be filename and its extension
+filename.txt
+filename.json
+
+# if there is anything in doubt, you can mention me in discord
+"""
+
 import os
 import shutil
 
