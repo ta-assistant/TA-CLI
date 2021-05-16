4af31a2b5f997b479f6ad30bfdc5e6900e47adc1
Vermouth02 <detectiveb4869@gmail.com>
2021-05-16 13:20:29 +0700

    Unzip def

--- lib/file_management/extract.py
+++ lib/file_management/extract.py
@@ -1,34 +1,23 @@
 import os
-
-A = "C:/Users/detec/Documents/Grid"
-path = A
-zip = os.listdir(path)
-count = 0
-print(zip)
-
-for i in zip :
-    count += 1
-print(count)
-
+import zipfile
 from file_management_lib import DirManagement
 
-create_dir = DirManagement().create_dir
+def list_file(path):
+    listfile = os.listdir(path)
+    return listfile
 
-import zipfile
+def unzipfile(listfile):
+    create_dir = DirManagement().create_dir
+    for i in listfile:
+        if ".zip" in i :
+            name = path + f"/{i}"
+            folder = name[0:-4]
+            with zipfile.ZipFile(name) as my_zip:
+                create_dir(folder)
+                my_zip.extractall(folder)
+                my_zip.close()
 
-for i in zip:
-    name = A+f"/{i}"
-    folder = name[0:-4]
-    print(name,folder)
-    with zipfile.ZipFile(name) as my_zip:
-        create_dir(folder)
-        my_zip.extractall(folder)
-        my_zip.close()
-
-
-#with zipfile.ZipFile(r"C:\Users\detec\Documents\Grid\Files.zip") as my_zip:
-#    create_dir(r"C:\Users\detec\Documents\Grid\Files")
-#    my_zip.extractall(r"C:\Users\detec\Documents\Grid\Files")
-#    my_zip.close()
-
+path = "C:/Users/detec/Documents/Grid"
+listfile = list_file(path)
+unzipfile(listfile)
     
