02e25e928addf32f3dd98dc373f7e1c06e3892a2
Vermouth02 <detectiveb4869@gmail.com>
2021-05-18 14:08:29 +0700

    Check valid file name

--- lib/file_management/extract.py
+++ lib/file_management/extract.py
@@ -11,11 +11,48 @@ unzipfile(path: str)
 import os
 import zipfile
 from file_management_lib import DirManagement
+from job_editor import JobEditor
+
+def check_filename_draft(filename: list,draft: list) -> bool:
+    if len(filename) != len(draft):
+        return False
+    else:
+        return True
+
+def check_valid_filename(path: str,filename: str) -> bool:
+    """check that zip filename is valid or not
+
+    Args:
+        path (str): path of work directory
+        filename (str): zip file name
+
+    Returns:
+        bool: if zip file name is valid return true else false
+    """
+    zdraft = JobEditor("").read_file(path+r"\ta\draft.json")
+    zdraft = zdraft["zip_file_draft"]
+    key=[]
+    reminder = ""
+    prejob = {}
+    for i in zdraft:
+        if i == "{":
+            reminder = ""
+        elif i == "}":
+            key.append(reminder)
+        else:
+            reminder += i
+    list_filename = filename.split("_")
+    if not check_filename_draft(list_filename,key):
+        print("Invalid file name " + filename)
+        return False
+    else:
+        return True
 
 def unzipfile(path: str):
     """
-    ###'path: str' is directory name that you want this function to extract files and create folders in this
-    ### You should to change backslash to sla for prevebt backslash error
+    'path: (str)' is directory name that you want this function to extract files and create folders in this
+    You should to change backslash to sla for prevebt backslash error
+    The floder's name where files extracted is same as the zip file's name.
     """
     listfile = os.listdir(path)
     create_dir = DirManagement().create_dir
@@ -23,13 +60,16 @@ def unzipfile(path: str):
         if ".zip" in i :
             name = path + f"/{i}"
             folder = name[0:-4]
-            with zipfile.ZipFile(name) as my_zip:
-                create_dir(folder)
-                my_zip.extractall(folder)
-                my_zip.close()
-                print(f"Successfully extracted files to {folder}" )
+            if check_valid_filename(path,folder):
+                with zipfile.ZipFile(name) as my_zip:
+                    create_dir(folder)
+                    my_zip.extractall(folder)
+                    my_zip.close()
+                    print(f"Successfully extracted files to {folder}")
+
 
 if __name__ ==  "__main__" :
     path = "C:/Users/detec/Documents/Grid"
     unzipfile(path)
+
     
