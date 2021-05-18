21bcefb79198ee4878d010d9f337e971c188d8a4
Vitvara Varavithya <gridxlgm@gmail.com>
2021-05-18 12:13:55 +0700

    new predata getfile name

--- src/main/pre_data.py
+++ src/main/pre_data.py
@@ -7,8 +7,35 @@ sys.path.insert(0,os.getcwd())
 
 from job_editor import JobEditor
 
-def get_file_name(path,filename):
-    zdraft = JobEditor("").read_file(path+r"\ta\draft.json")
-    print(zdraft["zip_file_draft"])
 
-get_file_name(r"C:\Users\Admin\Desktop\ex1","Asdfasdfsa")
+def check_filename_draft(filename: list,draft: list) -> bool:
+    if len(filename) != len(draft):
+        return False
+    else:
+        return True
+
+
+def get_file_name(path,filename) -> dict:
+    zdraft = JobEditor("").read_file(path+r"\ta\draft.json")
+    zdraft = zdraft["zip_file_draft"]
+    key=[]
+    reminder = ""
+    prejob = {}
+    for i in zdraft:
+        if i == "{":
+            reminder = ""
+        elif i == "}":
+            key.append(reminder)
+        else:
+            reminder += i
+    list_filename = filename.split("_")
+    if not check_filename_draft(list_filename,key):
+        print("Invalid file name " + filename)
+        return {}
+    for key,value in zip(key,filename):
+        prejob[key] = value
+
+    return prejob
+
+if __name__ == "__main__":
+    print(get_file_name(r"C:\Users\Admin\Desktop\ex1",r"6310546066_vitvara_ex1"))
