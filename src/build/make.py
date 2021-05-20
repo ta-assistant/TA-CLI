a2b92191c8111a685bc450237ac4b5689b6ea01c
Vitvara Varavithya <gridxlgm@gmail.com>
2021-05-20 14:01:38 +0700

    test make

--- src/build/make.py
+++ src/build/make.py
@@ -9,22 +9,33 @@ from job_editor import JobEditor
 from file_management_lib import DirManagement, FileEditor
 
 
+
+def create_draft() -> bool:
+    for i in range(8):
+        draft_init = input("Do you want to fetch draft.json from the server:(yes,no) ")
+        if draft_init.lower() == "yes":
+            return True
+        elif draft_init.lower() == "no":
+            return False
+
+
 def init_work_directory(path) -> bool:
     print(f"{path} makeing work directory")
     if os.path.exists(path+r"\ta"):
         print(path+" is already a work directory")
+        return False
     else:
         DirManagement().create_dir(path+r"\ta")
     print("Now you can add your draft.json")
-    for i in range(8):
-        draft_init = input("Do you want to fetch draft.json from the server:(yes,no) ")
-        if draft_init.lower() == "yes":
-            break
-        elif draft_init.lower() == "no":
-            break
+    if create_draft():
+        pass
     # extract function
     print(f"{path} is ready")
+    return True
 
+def reset(path):
+    DirManagement.remove_dir(path+r"\ta")
+    
 if __name__ == "__main__":
     init_work_directory(r"C:\Users\Admin\Desktop\ex1")
     
--- test/test_make.py
+++ test/test_make.py
@@ -7,11 +7,17 @@ sys.path.insert(0,os.getcwd())
 
 import make
 import unittest
+from unittest.mock import patch
 
 class TestMake(unittest.TestCase):
-    def test_make(self):
-        make.init_work_directory(currentdir)
-        make.init_work_directory(currentdir)
+    @patch("make.create_draft",return_value='yes')
+    def test_init_make(self,input):
+        self.assertTrue(make.init_work_directory(currentdir))
+        self.assertFalse(make.init_work_directory(currentdir))
+
+    def tearDown(self) -> None:
+        make.reset(currentdir)
+        return super().tearDown()
 
 if __name__ == "__main__":
     unittest.main()
