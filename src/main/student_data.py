990e113031e329f8f05e9d814f802f70c2032cb9
Vitvara Varavithya <gridxlgm@gmail.com>
2021-05-21 20:34:05 +0700

    fix path

--- src/main/student_data.py
+++ src/main/student_data.py
@@ -2,8 +2,7 @@ import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(os.path.dirname(currentdir))
 
-os.chdir(parentdir)
-sys.path.insert(0,os.getcwd())
+sys.path.insert(0,parentdir)
 
 from lib.file_management.file_management_lib import FileEditor, DirManagement,WorkEditor
 
