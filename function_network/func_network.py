7f409b98cf44c302c262ee4f3411b29d3a474de5
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-05-21 19:39:46 +0700

    starting code not modify yet

--- function_network/func_network.py
+++ function_network/func_network.py
@@ -1,8 +1,24 @@
 import requests
 import json
 
-# reponse = requests.get("{---}")
+class Path:
+    def find_path(start, api_path):
+        return f"{start}/{api_path}"
+
+hparameter = { 'Authorization': 'ApiKey' ,
+            'Content-Type': 'application/json',
+}
+
+dparameter = {
+    "workID":"testWork"
+}
+
+url = Path.find_path("https://google.com", "/v1/getWorkDraft/")
+reponse = requests.get(url, headers=hparameter, data=dparameter)
+print(reponse.status_code)
 
 
-def find_path(start, api_path):
-    return f"{start}/{api_path}"
+# class JS:
+#     def jprint(obj):
+#         text = json.dumps(obj, sort_keys=True)
+#         return text
