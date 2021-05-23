5dec4499a00c66fb26a910ddc74790f0ecc7f7bf
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-05-23 21:10:29 +0700

    finish all base code, add ex_workdraft.json

--- function_network/func_network.py
+++ function_network/func_network.py
@@ -4,24 +4,26 @@ import os
 
 
 class Call_api:
-    def __init__(self, apikey) -> None:
+    def __init__(self, apikey, id) -> None:
 
 
         self.hparameter = { 'Authorization': apikey,
                 'Content-Type': 'application/json',
         }
 
-
-        self.url = "https://ta-api.sirateek.dev/v1/workManagement/testWork/getWorkDraft"
+        self.id = id
+        self.prefix = "https://ta-api.sirateek.dev/"
+        self.apipath = f'v1/workManagement/{self.id}/getWorkDraft'
+        self.url = f'{self.prefix}{self.apipath}'
 
         self.res = self.response()
-        self.get_workdraft()
+        # self.createworkdraft()
 
     def response(self):
         res = requests.get(self.url, headers=self.hparameter)
         return res
 
-    def get_workdraft(self):
+    def createworkdraft(self):
         if self.res.status_code == 200:
             print('Success to access')    
             self.data = self.res.json()['workDraft']
@@ -46,7 +48,26 @@ class WriteWorkdaft:
             json.dump(self.work, create)
         print("workDraft.json file has been created")
 
+class SendData(Call_api):
+    def __init__(self, apikey, id) -> None:
+        super().__init__(apikey, id)
+        self.prefix = "https://ta-api.sirateek.dev/"
+        self.apipath = f'v1/workManagement/{self.id}/submitScores'
+        self.posturl = f'{self.prefix}{self.apipath}'
+        self.getworkDraft()
+        
 
+    def getworkDraft(self):
+        with open(os.getcwd()+'/function_network/ex_workdraft.json') as r:
+            workdraft = json.load(r)
+        send = requests.post(self.posturl, headers=self.hparameter, json=workdraft)
+        if send.status_code == 200:
+            print('Sending data success')
+        else:
+            print(send.status_code)
+            print(send.json())
+
+            
 if __name__ == "__main__":
-    Call_api('K4nPEs7RhhCzcjdlvr3X==')
-
+    Call_api('K4nPEs7RhhCzcjdlvr3X==', 'testWork2')
+    SendData('K4nPEs7RhhCzcjdlvr3X==', 'testWork2')
