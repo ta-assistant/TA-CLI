84491455764d4b5244f8e97da48490bbec2548c6
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-05-24 19:32:27 +0700

    fix bug

--- function_network/func_network.py
+++ function_network/func_network.py
@@ -1,26 +1,24 @@
 import requests
 import json
 import os
-import sys
 
 
-class Call_api:
-    def __init__(self, apikey, id) -> None:
+class CallApi:
+    def __init__(self, apikey, workID, path) -> None:
         self.apikey = apikey
+        self.path = path
         self.prefix = self.readprefix()
-
         self.hparameter = { 'Authorization': self.apikey,
                 'Content-Type': 'application/json',
         }
 
-        self.getapi = f"v1/workManagement/{id}/getWorkDraft"
-        
+        self.getapi = f"v1/workManagement/{workID}/getWorkDraft"
         self.url = self.prefix+self.getapi
         self.createworkdraft()
-
+        print()
     
     def readprefix(self):
-        p = open("function_network\config.txt", "r")
+        p = open(os.path.join(self.path, 'ta', 'config.txt'), "r")
         prefix = p.read().split()[2]
         p.close()
         return prefix
@@ -28,29 +26,25 @@ class Call_api:
     def createworkdraft(self):
         self.res = requests.get(self.url, headers=self.hparameter)
         if self.res.status_code == 200:
-            print('Success to access')    
+            print('Success to access')
             self.data = self.res.json()['workDraft']
             self.writejson(self.data)
         else:
+            print(self.res.status_code)
             print(self.res.json()['message'])
 
 
     def writejson(self, data) -> None:
-        if not os.path.exists(os.getcwd()+'/function_network'):
-            try:
-                os.makedirs(os.getcwd()+'/function_network')
-            except Exception as e:
-                print(e)
-                raise
-        with open(os.path.join(os.getcwd()+'/function_network', "workDraft.json"), "w") as create:
+        with open(os.path.join(self.path, 'ta', "draft.json"), "w") as create:
             json.dump(data, create)
         print("workDraft.json file has been created")
 
 
 class SendData:
-    def __init__(self, apikey, id) -> None:
+    def __init__(self, apikey, workID, path) -> None:
         self.apikey = apikey
-        p = open("function_network\config.txt", "r")
+        self.path = path
+        p = open(os.path.join(self.path, 'ta', 'config.txt'), "r")
         self.prefix = p.read().split()[2]
         p.close()
         self.hparameter = { 'Authorization': self.apikey,
@@ -58,13 +52,13 @@ class SendData:
         }
 
 
-        self.postapi = f"v1/workManagement/{id}/submitScores"
+        self.postapi = f"v1/workManagement/{workID}/submitScores"
         self.posturl = self.prefix+self.postapi
-        self.getworkDraft()
+
         
 
     def getworkDraft(self):
-        with open(os.getcwd()+'/function_network/ex_workdraft.json') as r:
+        with open(os.path.join(self.path, 'ta', 'work.json')) as r:
             workdraft = json.load(r)
         send = requests.post(self.posturl, headers=self.hparameter, json=workdraft)
         if send.status_code == 200:
@@ -75,5 +69,5 @@ class SendData:
 
             
 if __name__ == "__main__":
-    Call_api('K4nPEs7RhhCzcjdlvr3X==', 'testWork2')
-    SendData('K4nPEs7RhhCzcjdlvr3X==', 'testWork2')
+    CallApi('K4nPEs7RhhCzcjdlvr3X==', 'testWork2', r'C:\vs\ta\TA-CLI\testypath')
+    SendData('K4nPEs7RhhCzcjdlvr3X==', 'testWork2', r'C:\vs\ta\TA-CLI\testypath')
