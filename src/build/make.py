7f46d4672586061a2c2cc15db4a4f9d9f9a25326
vitvara <69972884+vitvara@users.noreply.github.com>
2021-05-26 15:47:40 +0700

    Feature ask follow draft (#21)
    * make file finish
    
    * add build
    
    * new predata getfile name
    
    * remove checkdraft
    
    * fix bug pre_data return only student id
    
    * init commit
    
    * post data update
    
    * post pre finish without document
    
    * fix miss spelling pre_data src
    
    * refactor main to student data
    
    * change from argparse to click
    
    * test make init
    
    * test make
    
    * test draft check
    
    * remove a old job editor
    
    * path bug found
    
    * student_data update
    
    * remove unuse code student_data.py
    
    * merge feature-CLI
    
    * refactor student data
    
    * add document draft student data make
    
    * change student_data.py follow workDraft
    
    * update file management lib to init work,json follow api
    
    * Change name from assist to ta
    
    * fix path
    
    * Done with ta init
    
    * workeditor workid fix
    
    * Merge
    
    * make workeditor import
    
    * finish reafactor student_data.py
    
    * Done and wait for Lung Dean
    
    * ready to merge
    
    * update extract
    
    * updata make
    
    * update check draft
    
    * update prework
    
    * update student data
    
    * updata file management lib
    
    * remove tacli and fix bug file management lib
    
    * remove cli file
    
    * update branch
    
    * finish make.py
    
    * remove unuse dir
    
    * test error fixed file manage ment lib
    
    * edit path send data test
    
    * fix test make
    
    Co-authored-by: abounk <a_bunk911@hotmail.com>

---
+++ src/build/make.py
@@ -0,0 +1,33 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(os.path.dirname(currentdir))
+sys.path.insert(0,parentdir)
+import json
+from lib.file_management.file_management_lib import DirManagement, FileEditor, WorkEditor
+from lib.function_network.func_network import CallApi
+def create_draft() -> bool:
+    for i in range(8):
+        draft_init = input("Do you want to fetch draft.json from the server:(yes,no) ")
+        if draft_init.lower() == "yes":
+            return True
+        elif draft_init.lower() == "no":
+            return False
+
+def init_work_directory(path) -> bool:
+    print(f"{path} makeing work directory")
+    ta_path = os.path.join(path,"ta")
+    if os.path.exists(ta_path):
+        print(path+" is already a work directory")
+    else:
+        DirManagement().create_dir(ta_path)
+    data = 'prefix = '
+    with open(os.path.join(ta_path, "config.txt"), "w") as create:
+        json.dump(data, create)
+        print("config.txt has been init.")
+        create.close()
+    print(f"{path} is ready")
+    return create_draft()
+
+def reset(path):
+    ta_path = os.path.join(path,"ta")
+    DirManagement.remove_dir(ta_path)
