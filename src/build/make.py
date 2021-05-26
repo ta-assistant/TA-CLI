e697f47857ca83a52f6e69c0ccd10d012a52ddc5
Vitvara Varavithya <gridxlgm@gmail.com>
2021-05-26 15:00:45 +0700

    finish make.py

--- src/build/make.py
+++ src/build/make.py
@@ -2,9 +2,9 @@ import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.insert(0,parentdir)
-
+import json
 from lib.file_management.file_management_lib import DirManagement, FileEditor, WorkEditor
-
+from lib.function_network.func_network import CallApi
 def create_draft() -> bool:
     for i in range(8):
         draft_init = input("Do you want to fetch draft.json from the server:(yes,no) ")
@@ -18,14 +18,15 @@ def init_work_directory(path) -> bool:
     ta_path = os.path.join(path,"ta")
     if os.path.exists(ta_path):
         print(path+" is already a work directory")
-        return False
     else:
         DirManagement().create_dir(ta_path)
-    print("Now you can add your draft.json")
-    if create_draft():
-        pass
+    data = 'prefix = '
+    with open(os.path.join(ta_path, "config.txt"), "w") as create:
+        json.dump(data, create)
+        print("config.txt has been init.")
+        create.close()
     print(f"{path} is ready")
-    return True
+    return create_draft()
 
 def reset(path):
     ta_path = os.path.join(path,"ta")
--- test/test_make.py
+++ test/test_make.py
@@ -2,30 +2,33 @@ import unittest
 import os,sys,inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-os.chdir(parentdir+r"\src\build")
-sys.path.insert(0,os.getcwd())
 
-import make
+sys.path.insert(0,parentdir)
+import json
+from src.build.make import init_work_directory, reset
+from lib.file_management.file_management_lib import DirManagement
 from unittest.mock import patch
 
+
+
 class TestMake(unittest.TestCase):
-    
-    @patch("make.create_draft",return_value='yes')
-    def test_init_make(self,input):
+    @patch("src.build.make.create_draft",return_value='yes')
+    def test_init_make(self):
         """
         call init_work_directory to set test to work dir
         Args:
             input (str): "yes"
         """
-        self.assertTrue(make.init_work_directory(currentdir))
-        self.assertFalse(make.init_work_directory(currentdir))
+        self.assertTrue(init_work_directory(currentdir))
+        self.assertFalse(init_work_directory(currentdir))
+        
 
-    def tearDown(self) -> None:
-        """
-        when test finish reset test into normal dir (not work directory)
-        """
-        make.reset(currentdir)
-        return super().tearDown()
+    # def tearDown(self) -> None:
+    #     """
+    #     when test finish reset test into normal dir (not work directory)
+    #     """
+    #     reset(currentdir)
+    #     return super().tearDown()
 
 if __name__ == "__main__":
     unittest.main()
