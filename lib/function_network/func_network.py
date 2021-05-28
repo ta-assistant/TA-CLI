37b0095917e50af010bb48b3b43bf878707c4e6e
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-05-28 13:20:15 +0700

    fix

--- lib/function_network/func_network.py
+++ lib/function_network/func_network.py
@@ -3,25 +3,48 @@ import json
 import os
 from configparser import ConfigParser
 
-class CallApi:
-    def __init__(self, apikey, workID, path) -> None:
+class Writeconfig:
+    def __init__(self, workID, path) -> None:
+        self.id = workID
+        self.basepath = path
+        self.writeworkid_path()
+        
+
+    def writeworkid_path(self):
+        with open(os.path.join(self.basepath, 'ta', 'config.txt'), "w") as wri:
+            wri.write("[CONFIG]\nprefix = https://ta-api.sirateek.dev/")
+            wri.write(f'\nworkID = {self.id}')
+            print("config.txt has been create")
+            wri.close()
+
+
+class ReadFile:
+    def fileread(self, path, filename):
+        if filename == 'config.txt':
+            config = ConfigParser() 
+            configFilePath = os.path.join(path ,'ta', filename)
+            config.read_file(open(configFilePath))
+            return config.get("CONFIG","prefix"), config.get("CONFIG","workID")
+        else:
+            with open(os.path.join(path, 'ta', filename)) as r:
+                return r.read()
+
+
+
+class CallApi(ReadFile):
+    def __init__(self, apikey, path) -> None:
         self.apikey = apikey
         self.path = path
-        self.prefix = self.readprefix()
+        self.prefix, self.workid = self.fileread(os.path.join(self.path), 'config.txt')
         self.hparameter = { 'Authorization': self.apikey,
                 'Content-Type': 'application/json',
         }
 
-        self.getapi = f"v1/workManagement/{workID}/getWorkDraft"
+        self.getapi = f"v1/workManagement/{self.workid}/getWorkDraft"
         self.url = self.prefix+self.getapi
         self.createworkdraft()
         print()
-    
-    def readprefix(self):
-        config = ConfigParser() 
-        configFilePath = os.path.join(self.path,"ta","config.txt")
-        config.readfp(open(configFilePath))
-        return config.get("CONFIG","prefix")
+
 
     def createworkdraft(self):
         self.res = requests.get(self.url, headers=self.hparameter)
@@ -39,34 +62,29 @@ class CallApi:
             json.dump(data, create)
         print("workDraft.json file has been created")
 
+    
 
-class SendData:
-    def __init__(self, apikey, workID, path) -> None:
+class SendData(ReadFile):
+    def __init__(self, apikey, path) -> None:
         self.apikey = apikey
         self.path = path
-        config = ConfigParser() 
-        configFilePath = os.path.join(self.path,"ta","config.txt")
-        config.readfp(open(configFilePath))
-        self.prefix = config.get("CONFIG","prefix")
+        self.prefix, self.workID = self.fileread(self.path, 'config.txt')
         self.hparameter = { 'Authorization': self.apikey,
                 'Content-Type': 'application/json',
         }
 
 
-        self.postapi = f"v1/workManagement/{workID}/submitScores"
+        self.postapi = f"v1/workManagement/{self.workID}/submitScores"
         self.posturl = self.prefix+self.postapi
         self.getworkDraft()
         
 
     def getworkDraft(self):
-        with open(os.path.join(self.path, 'ta', 'work.json')) as r:
-            workdraft = json.load(r)
-        send = requests.post(self.posturl, headers=self.hparameter, json=workdraft)
+        work = self.fileread(self.path, 'work.json')
+        send = requests.post(self.posturl, headers=self.hparameter, json=json.loads(work))
         if send.status_code == 200:
             print('Sending data success')
         else:
             print(send.status_code)
             print(send.json())
-
-            
 
