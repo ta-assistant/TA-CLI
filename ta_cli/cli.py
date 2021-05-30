ba1f4b817b842b3ef63bffdb3218b84a4a2d7e83
abounk <a_bunk911@hotmail.com>
2021-05-30 23:13:47 +0700

    Commit for merge

--- ta_cli/cli.py
+++ ta_cli/cli.py
@@ -10,6 +10,7 @@ from src.main.student_data import StudentData
 from src.main.pre_work import Work
 from lib.file_management.extract import unzipfile
 from lib.function_network.func_network import CallApi
+from lib.file_management.configeditor import ConfigEditor
 
 
 @click.command()
@@ -19,32 +20,29 @@ from lib.function_network.func_network import CallApi
 def cli(init, start, fetch):
     current_dir = os.getcwd()
     if init:
-        apikey, workid = init
+        apikey, workID = init
         if make.init_work_directory(current_dir):
-            fetch_draft = CallApi(apikey, workid, current_dir)
-            # with open(os.path.join(current_dir, "ta", "config.txt"), "r+") as configfile:
-            #     configfile.write(f"\nworkID = {workid}\napiKey = {apikey}")
-            #     configfile.close()
-    if start:
-        if check_draft(current_dir):
-            with open(os.path.join(current_dir, "ta", "draft.json")) as file:
-                draft = json.load(file)
-                file.close()
+            config = ConfigEditor(workID, current_dir)
+            call_api = CallApi(apikey, current_dir)
+            if check_draft(current_dir):
+                with open(os.path.join(current_dir, "ta", "draft.json")) as file:
+                    draft = json.load(file)
+                    file.close()
             work = Work()
             work.draft = draft
             work.path = current_dir
-            click.echo(current_dir)
-            work.workId = 6310546031
+            work.workId = workID
             if work.property_is_ready():
-                unzipfile(current_dir)
                 work.create()
-                list_file = os.listdir(current_dir)
-                command = f"code {current_dir}"
-                os.system(command)
-                for file in list_file:
-                    if "." in file:
-                        continue
-                    student = StudentData(
-                        path=work.path, filename=file, draft=work.draft)
-                    student.prepare_student_data()
-                    work.write_work(student.ask())
+    if start:
+        unzipfile(current_dir)
+        list_file = os.listdir(current_dir)
+        command = f"code {current_dir}"
+        os.system(command)
+        for file in list_file:
+            if "." in file:
+                continue
+            student = StudentData(
+                path=work.path, filename=file, draft=work.draft)
+            student.prepare_student_data()
+            work.write_work(student.ask())
