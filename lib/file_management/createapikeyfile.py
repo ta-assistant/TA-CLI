625ea93f9f57a3d9f752d72977b90d5e403f067c
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-06-02 16:35:02 +0700

    change dir to save apikey

--- lib/file_management/createapikeyfile.py
+++ lib/file_management/createapikeyfile.py
@@ -8,15 +8,12 @@ class SaveApiKey:
 
     def save(self, apikey):
         data = {'apikey' : apikey}
-        with open(os.path.join(self.path, 'ta', 'apik.json'), "w") as wri:
+        os.mkdir(os.path.join(self.path, 'key'))
+        with open(os.path.join(self.path, 'key', 'apik.json'), "w") as wri:
             json.dump(data, wri)
 
     def readapikey(self):
-        with open(os.path.join(self.path, 'ta', "apik.json"), "r") as r:
+        with open(os.path.join(self.path, 'key', "apik.json"), "r") as r:
             data = json.load(r)
         return data
 
-
-if __name__ == '__main__':
-    a = SaveApiKey(r'C:\vs\ta\TA-CLI\testypath')
-    a.save('K4nPEs7RhhCzcjdlvr3X==')
