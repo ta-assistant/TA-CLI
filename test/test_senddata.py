76109a75f7566819eca7f60ed0e60ef01fc9bdec
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-06-05 20:53:16 +0700

    fix test

--- test/test_senddata.py
+++ test/test_senddata.py
@@ -35,7 +35,6 @@ class TestSendData(unittest.TestCase):
             json.dump(data, wri)
         with open(os.path.join(self.path, "work.json"), "w") as create:
             json.dump(workdata, create)
-        SaveApiKey().save('K4nPEs7RhhCzcjdlvr3X==')
         self.post = SendData(parentdir)
         return super().setUp()
 
@@ -43,6 +42,7 @@ class TestSendData(unittest.TestCase):
         """
         return None
         """
+        SaveApiKey().save('K4nPEs7RhhCzcjdlvr3X==')
         self.assertIsNone(self.post.getworkDraft())
 
 
