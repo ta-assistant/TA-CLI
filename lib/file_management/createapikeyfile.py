da935dc30471ad34bdee5ef0294549196954b119
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-06-05 20:44:48 +0700

    add code to handle when already have apikey

--- lib/file_management/createapikeyfile.py
+++ lib/file_management/createapikeyfile.py
@@ -1,21 +1,53 @@
 import json
 import os
+import shutil
 
 class SaveApiKey:
 
     def save(self, apikey):
         if os.path.exists(os.path.join(os.path.expanduser("~"), 'key')):
-            data = {'apikey' : apikey}
-            with open(os.path.join(os.path.expanduser("~"), 'key', 'apik.json'), "w") as wri:
-                json.dump(data, wri)
+            if self.exsitapikey():
+                print('Already Login')
+                choose = input('Do you want login again[y/n]: ')
+                if choose == 'y' or choose == 'Y':
+                    self.writeapikey(apikey)
+                    print(os.path.join(os.path.expanduser("~"), 'key')+" has been written.")
+                elif choose == 'n' or choose == 'N':
+                    print('Decline to login again')
+            else:
+                self.writeapikey(apikey)
+                print(os.path.join(os.path.expanduser("~"), 'key')+" has been written.")
         else:
             os.mkdir(os.path.join(os.path.expanduser("~"), 'key'))
-            data = {'apikey' : apikey}
-            with open(os.path.join(os.path.expanduser("~"), 'key', 'apik.json'), "w") as wri:
-                json.dump(data, wri)
+            self.writeapikey(apikey)
+            print(os.path.join(os.path.expanduser("~"), 'key')+" has been created.")
 
     def readapikey(self):
         with open(os.path.join(os.path.expanduser("~"), 'key', "apik.json"), "r") as r:
             data = json.load(r)
         return data['apikey']
 
+    def writeapikey(self, apikey):
+        data = {'apikey' : apikey}
+        with open(os.path.join(os.path.expanduser("~"), 'key', 'apik.json'), "w") as wri:
+            json.dump(data, wri)
+            wri.close()
+
+    def exsitapikey(self):
+        if os.path.exists(os.path.join(os.path.expanduser("~"), 'key', 'apik.json')):
+            return True
+        else:
+            return False
+
+    def removeapikey(self):
+        try:
+            shutil.rmtree(os.path.join(os.path.expanduser("~"), 'key'))
+        except OSError:
+            print("Deletion of the directory %s failed" % os.path.join(os.path.expanduser("~"), 'key'))
+        else:
+            print("Deletion of the directory %s success" % os.path.join(os.path.expanduser("~"), 'key'))
+
+
+if __name__ == '__main__':
+    a = SaveApiKey()
+    a.save('K4nPEs7RhhCzcjdlvr3X==')
