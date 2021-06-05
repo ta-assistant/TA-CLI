ea1a7a25dc86621e38ecef05ea4158ab7f424ce4
abounk <a_bunk911@hotmail.com>
2021-06-05 17:40:36 +0700

    Refactor completed

--- ta_cli/cli.py
+++ ta_cli/cli.py
@@ -12,6 +12,8 @@ from lib.file_management.extract import unzipfile
 from lib.function_network.func_network import CallApi, SendData
 from lib.file_management.configeditor import ConfigEditor
 
+current_dir = os.getcwd()
+
 
 def read_json(path, filename):
     with open(os.path.join(path, "ta", filename)) as file:
@@ -20,42 +22,109 @@ def read_json(path, filename):
     return res
 
 
-@click.command()
-@click.option("--init", nargs=2, type=str, help="Init TA's work directory")
-@click.option("--start", is_flag=True, help="Start")
-@click.option("--fetch", is_flag=True, help="Fetch draft.json")
-@click.option("--submit", nargs=1, type=str, help="Submit work.json")
-def cli(init, start, fetch, submit):
-    current_dir = os.getcwd()
-    if init:
-        apikey, workID = init
-        if make.init_work_directory(current_dir):
-            ConfigEditor(workID, current_dir)
-            CallApi(apikey, current_dir)
+@click.group()
+def cli():
+    pass
 
-    if start:
-        if check_draft(current_dir):
-            draft = read_json(current_dir, "draft.json")
-            workID = read_json(current_dir, "config.json")["workID"]
-        work = Work()
-        work.draft = draft
-        work.path = current_dir
-        work.workId = workID
-        if work.property_is_ready():
-            work.create()
-        unzipfile(current_dir)
-        list_file = os.listdir(current_dir)
-        # click.echo(list_file)
-        for file in list_file:
-            if "." in file or file == "ta":
-                continue
-            # click.echo(file)
-            os.system(f"code {file}")
-            student = StudentData(
-                path=work.path, filename=file, draft=work.draft)
-            student.prepare_student_data()
-            work.write_work(student.ask())
 
-    if submit:
-        apikey = submit
-        SendData(apikey, current_dir)
+@cli.command()
+@click.option("--username", prompt="Username\n")
+@click.option("--password", prompt="Password\n", hide_input=True)
+def login(username, password):
+    """Login"""
+    pass
+
+
+@cli.command()
+@click.argument("apikey", type=str)
+@click.argument("workID", type=str)
+def init(apikey, workID):
+    """Init TA's work directory
+
+    Args:
+        apikey (str): Api key
+        workID (str): Work's ID
+    """
+    if make.init_work_directory(current_dir):
+        ConfigEditor(workID, current_dir)
+        CallApi(apikey, current_dir)
+
+
+@cli.command()
+def start():
+    """Start working on TA directory"""
+    if check_draft(current_dir):
+        draft = read_json(current_dir, "draft.json")
+        workID = read_json(current_dir, "config.json")["workID"]
+    work = Work()
+    work.draft = draft
+    work.path = current_dir
+    work.workId = workID
+    if work.property_is_ready():
+        work.create()
+    unzipfile(current_dir)
+    list_file = os.listdir(current_dir)
+    # click.echo(list_file)
+    for file in list_file:
+        if "." in file or file == "ta":
+            continue
+        # click.echo(file)
+        os.system(f"code {file}")
+        student = StudentData(
+            path=work.path, filename=file, draft=work.draft)
+        student.prepare_student_data()
+        work.write_work(student.ask())
+
+
+@cli.command()
+def fetch():
+    """Fetch draft.json"""
+    pass
+
+
+@cli.command()
+@click.argument("apikey", type=str)
+def submit(apikey):
+    """Submit"""
+    SendData(apikey, current_dir)
+
+
+# @click.command()
+# @click.option("--init", nargs=2, type=str, help="Init TA's work directory")
+# @click.option("--start", is_flag=True, help="Start")
+# @click.option("--fetch", is_flag=True, help="Fetch draft.json")
+# @click.option("--submit", nargs=1, type=str, help="Submit work.json")
+# def cli(init, start, fetch, submit):
+#     current_dir = os.getcwd()
+#     if init:
+#         apikey, workID = init
+#         if make.init_work_directory(current_dir):
+#             ConfigEditor(workID, current_dir)
+#             CallApi(apikey, current_dir)
+
+#     if start:
+#         if check_draft(current_dir):
+#             draft = read_json(current_dir, "draft.json")
+#             workID = read_json(current_dir, "config.json")["workID"]
+#         work = Work()
+#         work.draft = draft
+#         work.path = current_dir
+#         work.workId = workID
+#         if work.property_is_ready():
+#             work.create()
+#         unzipfile(current_dir)
+#         list_file = os.listdir(current_dir)
+#         # click.echo(list_file)
+#         for file in list_file:
+#             if "." in file or file == "ta":
+#                 continue
+#             # click.echo(file)
+#             os.system(f"code {file}")
+#             student = StudentData(
+#                 path=work.path, filename=file, draft=work.draft)
+#             student.prepare_student_data()
+#             work.write_work(student.ask())
+
+#     if submit:
+#         apikey = submit
+#         SendData(apikey, current_dir)
