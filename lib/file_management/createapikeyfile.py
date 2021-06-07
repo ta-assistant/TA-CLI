f52dcf3440fccd84e8a75f71c5922a02f0649b65
Tanin_dean <70503441+Tanidean@users.noreply.github.com>
2021-06-07 19:28:04 +0700

    Refactor func network (#39)
    * first commit
    
    * starting code not modify yet
    
    * change to oop
    
    * create statring code for create WorkDaft.json
    
    * base code for callapi,write workDaft.json
    
    * correct workDraft's file name
    
    * finish all base code, add ex_workdraft.json
    
    * delete commend code
    
    * fix bug
    
    * create config.py to collect prefix url
    
    * change config.py to config.ttx
    
    * Delete config.py
    
    * merge Writeworkdraft in to Call_api
    
    * change function name create_work -> writejson
    
    * create readprefix method
    
    * fix bug
    
    * add test class callapi, class senddata
    
    * move func_network.py dir to lib\management
    
    * change dir of func_network.pt
    
    * Delete function_network directory
    
    * remove import from 2 test
    
    * fix error
    
    * fix test error
    
    * fix test error
    
    * first commit
    
    * create new class to handle readfile and change someunitest
    
    * finish refactor
    
    * delete some import that didt use
    
    * add class to create config + add test ofthat class
    
    * fix
    
    * Delete work.json
    
    * fix
    
    * Delete lib/file_management/function_network directory
    
    * move Writeconfig class to function in callapi, change class readfile
    
    * change write class to new file in file_namnagement, create test for configeditor
    
    * create class Api for make superclass for callapi and senddata, delete import that didnt use in testcall api
    
    * move postapi and posturl to Api class
    
    * change create config.txt to config.jsob
    
    * fix test callapi
    
    * add check config file
    
    * create file to saveapikey, test class saveapikey
    
    * fix test and some test line code
    
    * change dir to save apikey
    
    * set spacific apikey dir0
    
    * fix test
    
    * fix test and support server issue for unites
    
    * fix senddata test
    
    * fix test
    
    * change return only apikey not dict
    
    * fix test
    
    * delete some useless line
    
    * add code to handle when already have apikey
    
    * add test of new code
    
    * fix test
    
    * fix
    
    * fix test
    
    * add print wrong answer input
    
    * change print invalid
    
    Co-authored-by: vitvara <69972884+vitvara@users.noreply.github.com>

--- lib/file_management/createapikeyfile.py
+++ lib/file_management/createapikeyfile.py
@@ -1,24 +1,52 @@
 import json
 import os
+import shutil
+
 
 class SaveApiKey:
 
     def save(self, apikey):
         if os.path.exists(os.path.join(os.path.expanduser("~"), 'key')):
-            data = {'apikey' : apikey}
-            with open(os.path.join(os.path.expanduser("~"), 'key', 'apik.json'), "w") as wri:
-                json.dump(data, wri)
-            print(os.path.join(os.path.expanduser("~"), 'key')+" has been written.")
+            if self.exsitapikey():
+                print('Already Login')
+                choose = input('Do you want login again[y/n]: ')
+                if choose == 'y' or choose == 'Y':
+                    self.writeapikey(apikey)
+                    print(os.path.join(os.path.expanduser("~"), 'key')+" has been written.")
+                elif choose == 'n' or choose == 'N':
+                    print('Decline to login')
+                else:
+                    print('!!!Invaild Answer!!!')
+            else:
+                self.writeapikey(apikey)
+                print(os.path.join(os.path.expanduser("~"), 'key')+" has been written.")
         else:
             os.mkdir(os.path.join(os.path.expanduser("~"), 'key'))
-            data = {'apikey' : apikey}
-            with open(os.path.join(os.path.expanduser("~"), 'key', 'apik.json'), "w") as wri:
-                json.dump(data, wri)
-                wri.close()
+            self.writeapikey(apikey)
             print(os.path.join(os.path.expanduser("~"), 'key')+" has been created.")
 
     def readapikey(self):
         with open(os.path.join(os.path.expanduser("~"), 'key', "apik.json"), "r") as r:
             data = json.load(r)
         return data['apikey']
+
+    def writeapikey(self, apikey):
+        data = {'apikey' : apikey}
+        with open(os.path.join(os.path.expanduser("~"), 'key', 'apik.json'), "w") as wri:
+            json.dump(data, wri)
+            wri.close()
+
+    def exsitapikey(self):
+        if os.path.exists(os.path.join(os.path.expanduser("~"), 'key', 'apik.json')):
+            return True
+        else:
+            return False
+
+    def removeapikey(self):
+        try:
+            shutil.rmtree(os.path.join(os.path.expanduser("~"), 'key'))
+        except OSError:
+            print("Deletion of the directory %s failed" % os.path.join(os.path.expanduser("~"), 'key'))
+        else:
+            print("Deletion of the directory %s success" % os.path.join(os.path.expanduser("~"), 'key'))
 
--- test/test_createapikeyfile.py
+++ test/test_createapikeyfile.py
@@ -12,7 +12,6 @@ class TestSendData(unittest.TestCase):
         self.path = os.path.join(os.path.expanduser("~") ,"key")
         DirManagement.create_dir(self.path)
         self.key = SaveApiKey()
-        self.key.save('K4nPEs7RhhCzcjdlvr3X==')
         return super().setUp()
 
     def testsave(self):
@@ -25,7 +24,28 @@ class TestSendData(unittest.TestCase):
         """
         return ...
         """
+        self.key.save('K4nPEs7RhhCzcjdlvr3X==')
         self.assertIs(type(self.key.readapikey()), str)
+
+    def testriteapikey(self):
+        """
+        return None
+        """
+        self.assertIsNone(self.key.writeapikey('K4nPEs7RhhCzcjdlvr3X=='))
+
+    def testexistapikey(self):
+        """
+        return boolean
+        """
+        self.assertFalse(self.key.exsitapikey())
+        self.key.save('K4nPEs7RhhCzcjdlvr3X==')
+        self.assertTrue(self.key.exsitapikey())
+
+    def testremoveapikey(self):
+        """
+        return None
+        """
+        self.assertIsNone(self.key.removeapikey())
 
     def tearDown(self) -> None:
         DirManagement.remove_dir(self.path)
