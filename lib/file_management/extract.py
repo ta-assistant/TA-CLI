89fde3fe7936d1678049a672598be8cae9555651
Vitvara Varavithya <gridxlgm@gmail.com>
2021-06-16 12:42:59 +0700

    finish loading bar

--- lib/file_management/extract.py
+++ lib/file_management/extract.py
@@ -7,8 +7,10 @@ unzipfile(path: str)
 
 import zipfile
 from lib.file_management.file_management_lib import DirManagement, WorkEditor, FileEditor
+from lib.file_management.loadin_bar import progressBar
 import inspect
 import sys
+import shutil
 import os
 
 
@@ -19,14 +21,19 @@ def unzipfile(path: str):
     The floder's name where files extracted is same as the zip file's name.
     """
     listfile = os.listdir(path)
+    if "ta" in listfile:
+        listfile.remove("ta")
     create_dir = DirManagement().create_dir
-    for filename in listfile:
+    out= " "*100
+    for filename in progressBar(listfile,prefix = 'Unzip progress:', suffix = 'complete ', length = 20):
         if ".zip" in filename:
             name = os.path.join(path, f"{filename}")
-            folder = name[0:-4]
+            folder = os.path.join(path, "ta","extract",f"{filename}")
             with zipfile.ZipFile(name) as my_zip:
-                create_dir(folder)
-                my_zip.extractall(folder)
-                my_zip.close()
-                print(f"Successfully extracted files to {folder}")
-            FileEditor.delete_file(path, filename)
+                try:
+                    create_dir(folder,out=False)
+                    my_zip.extractall(folder)
+                    my_zip.close()
+                except (IOError, zipfile.BadZipfile) as e:
+                    print(f"\rBad zip file given as {name}.{out}\n")
+                    shutil.rmtree(folder)
