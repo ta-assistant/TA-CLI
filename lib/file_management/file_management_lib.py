605095f0560e32ac74ab57d959b705885e65d65f
vitvara <gridxlgm@gmail.com>
2021-07-11 14:12:56 +0700

    fix bug when work draft is the same with selected draft

--- lib/file_management/file_management_lib.py
+++ lib/file_management/file_management_lib.py
@@ -66,17 +66,16 @@ class WorkEditor(FileEditor):
 
     def create_file_work(self, path, config, draft) -> bool:
         if not self.check_exits_work(path):
-    
             self.init_work(path)
             self.add_workid(path, config)
             self.add_draft(path, draft)
             return True
         else:
-            self.check_work_draft(path,draft,config)
-            while True:
-                if not os.path.exists(os.path.join(path,"ta","work.json")):
-                    self.create_file_work()
-                    break
+            if self.check_work_draft(path,draft,config):
+                while True:
+                    if not os.path.exists(os.path.join(path,"ta","work.json")):
+                        self.create_file_work()
+                        break
             return False
 
     def write_work(self, path, stu_data: dict) -> bool:
@@ -134,9 +133,10 @@ class WorkEditor(FileEditor):
     def check_work_draft(self,path,draft,config):
         workDraft = self.read_filework(path)
         if draft == workDraft["workDraft"]:
-            pass
+            return False
         else:
             os.remove(os.path.join(path,"ta","work.json"))
+            return True
             
 
             
