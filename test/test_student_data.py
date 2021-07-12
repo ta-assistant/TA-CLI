3bb6c5b7fcccbd03e172c5542bdb453e64387b63
abounk <a_bunk911@hotmail.com>
2021-07-13 00:44:11 +0700

    .

--- test/test_student_data.py
+++ test/test_student_data.py
@@ -1,82 +1,54 @@
+from src.main.student_data import StudentData
 import unittest
 from unittest.mock import patch
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+import os
+import sys
+import inspect
+currentdir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-<<<<<<< HEAD
-maindir = os.path.join(parentdir,"src","main")
-sys.path.insert(0,maindir)
 
-import student_data
-=======
-
-sys.path.insert(0,parentdir)
-
-from src.main.student_data import StudentData
->>>>>>> master
+sys.path.insert(0, parentdir)
 
 
 class TestStuData(unittest.TestCase):
     def setUp(self) -> None:
-<<<<<<< HEAD
-        self.draft = {"workDraft": {
-=======
         self.draft = {
->>>>>>> master
-      "fileDraft": "{student_id}_{name}_{ex}.zip",
-      "outputDraft": [
-        "student_id",
-        "name",
-        "ex",
-        "score1",
-        "score2",
-        "comment"
-      ]
-<<<<<<< HEAD
-    }}
-
-
-        return super().setUp()
-    @patch("student_data.inask",return_value='1')
-=======
-    }
-
+            "fileDraft": "{student_id}_{name}_{ex}.zip",
+            "outputDraft": [
+                "student_id",
+                "name",
+                "ex",
+                "score1",
+                "score2",
+                "comment"
+            ]
+        }
 
         return super().setUp()
 
-    @patch("src.main.student_data.inask",return_value='1')
->>>>>>> master
-    def test_add_student_data(self,input):
+    @patch("src.main.student_data.inask", return_value='1')
+    def test_add_student_data(self, input):
         """
         test that when we have perfect env then it will produce a perfect data that we need
         """
-    
-<<<<<<< HEAD
-        stu1 = student_data.StudentData(currentdir,"6310546066_vitvara_ex1",self.draft)
-        stu1.draft_file = "{student_id}_{name}_{ex}.zip"
-=======
-        stu1 = StudentData(currentdir,"6310546066_vitvara_ex1",self.draft)
->>>>>>> master
-        stu1.prepare_student_data()
-        self.assertEqual(stu1.pre_data,{'student_id': '6310546066', 'name': 'vitvara', 'ex': 'ex1', 'score1': 'N/A', 'score2': 'N/A', 'comment': 'N/A'})
-        self.assertEqual(stu1.ask(),{'student_id': '6310546066', 'name': 'vitvara', 'ex': 'ex1', 'score1': '1', 'score2': '1', 'comment': '1'})
 
-<<<<<<< HEAD
-    @patch("student_data.inask",return_value='-99')
-    def test_skip(self,input):
-        stu1 = student_data.StudentData(currentdir,"6310546066_vitvara_ex1",self.draft)
-        stu1.draft_file = "{student_id}_{name}_{ex}.zip"
+        stu1 = StudentData(currentdir, "6310546066_vitvara_ex1", self.draft)
         stu1.prepare_student_data()
-        self.assertEqual(stu1.pre_data,{'student_id': '6310546066', 'name': 'vitvara', 'ex': 'ex1', 'score1': 'N/A', 'score2': 'N/A', 'comment': 'N/A'})
-        self.assertEqual(stu1.ask(),{'student_id': '6310546066', 'name': 'vitvara', 'ex': 'ex1', 'score1': 'N/A', 'score2': 'N/A', 'comment': 'N/A'})
-=======
-    @patch("src.main.student_data.inask",return_value='-99')
-    def test_skip(self,input):
-        stu1 = StudentData(currentdir,"6310546066_vitvara_ex1",self.draft)
+        self.assertEqual(stu1.pre_data, {'student_id': '6310546066', 'name': 'vitvara',
+                                         'ex': 'ex1', 'score1': 'N/A', 'score2': 'N/A', 'comment': 'N/A'})
+        self.assertEqual(stu1.ask(), {'student_id': '6310546066', 'name': 'vitvara',
+                                      'ex': 'ex1', 'score1': '1', 'score2': '1', 'comment': '1'})
+
+    @patch("src.main.student_data.inask", return_value='-99')
+    def test_skip(self, input):
+        stu1 = StudentData(currentdir, "6310546066_vitvara_ex1", self.draft)
         stu1.prepare_student_data()
-        self.assertEqual(stu1.pre_data,{'student_id': '6310546066', 'name': 'vitvara', 'ex': 'ex1', 'score1': 'N/A', 'score2': 'N/A', 'comment': 'N/A'})
-        self.assertEqual(stu1.ask(),{'student_id': '6310546066', 'name': 'vitvara', 'ex': 'ex1', 'score1': 'N/A', 'score2': 'N/A', 'comment': 'N/A'})
-        
->>>>>>> master
+        self.assertEqual(stu1.pre_data, {'student_id': '6310546066', 'name': 'vitvara',
+                                         'ex': 'ex1', 'score1': 'N/A', 'score2': 'N/A', 'comment': 'N/A'})
+        self.assertEqual(stu1.ask(), {'student_id': '6310546066', 'name': 'vitvara',
+                                      'ex': 'ex1', 'score1': 'N/A', 'score2': 'N/A', 'comment': 'N/A'})
+
+
 if __name__ == "__main__":
     unittest.main()
