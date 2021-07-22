2288b50152c090131f51f6de2de55baf32b3c9bf
vitvara <gridxlgm@gmail.com>
2021-07-22 14:51:14 +0700

    add ta comment to lib

---
+++ lib/ta_comment/main.py
@@ -0,0 +1,26 @@
+from check_comment import TAComment
+import os
+import json
+def ta_comment(path):
+    std_list = os.listdir(path)
+    comments = {"comments":[]}
+    container = {}
+    traceback = ""
+    for studir in std_list:
+        container[studir] = []
+        for directory, subdirectories, files in os.walk(os.path.join(path,studir)):
+            for file in files:
+                with open(os.path.join(directory, file),"r") as readfile:
+                    filedata = readfile.readlines()
+                TA = TAComment(filedata)
+                container[studir].append({file:TA.run()}) 
+                traceback += "=========================\n"
+                traceback += f"{studir}  {file}\n"
+                traceback += "=========================\n"
+                traceback += TA.out
+    print(traceback)      
+    return json.dumps(container,indent=3)
+
+
+with open('readme.txt', 'w') as f:
+    f.write(ta_comment("test1"))
