e5b0818052fe6b5cf6d250647a499553fb8dba44
vitvara <gridxlgm@gmail.com>
2021-07-24 20:02:39 +0700

    add document on file management init

--- lib/file_management/__init__.py
+++ lib/file_management/__init__.py
@@ -1,3 +1,38 @@
+"""
+File management
+===============
+
+Provides
+    1. Make it easier to send requests to the server.
+    2. Easy to manage file.
+    3. Make student work more systematic.
+
+How to use the documentation
+----------------------------
+Documentation is available in two forms: docstrings provided
+with the code, and a reference guide, available in README.md
+
+Available packages
+---------------------
+WorkEditor
+    Manage data in work.json
+
+ConfigEditor
+    [description]
+
+SaveApiKey  
+    [description]
+
+FileEditor
+    Read, Write, and Delete file this method can do it.
+
+DirManagement
+    Create and Remove directory
+
+manage_work_file
+    Manage and check student work
+"""
+
 from .configeditor import *
 from .createapikeyfile import *
 from .manage_work_file import *
