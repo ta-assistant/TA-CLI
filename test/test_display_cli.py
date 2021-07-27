c318d2fa4207b8494535574a22d6f7794cffb9fb
vitvara <gridxlgm@gmail.com>
2021-07-27 15:35:10 +0700

    add test display cli display status symbol

--- test/test_display_cli.py
+++ test/test_display_cli.py
@@ -16,12 +16,45 @@ class TestDisplayStatusSymbol(unittest.TestCase):
         sys.stdout = sys.__stdout__
         return capturedOutput.getvalue()
 
+    def _get_io_symbol_test(self,symbol):
+        # create StringIO object
+        capturedOutput = io.StringIO()
+        # Redirect stdout
+        sys.stdout = capturedOutput
+        # call function
+        display_status_symbol(0,symbol,"test symbol")
+        # reset redirect.
+        sys.stdout = sys.__stdout__
+        return capturedOutput.getvalue()
+
+    def _get_io_order_symbol_test(self,order,symbol):
+        # create StringIO object
+        capturedOutput = io.StringIO()
+        # Redirect stdout
+        sys.stdout = capturedOutput
+        # call function
+        display_status_symbol(order,symbol,"test oreder and symbol")
+        # reset redirect.
+        sys.stdout = sys.__stdout__
+        return capturedOutput.getvalue()
+
     def test_display_status_symbol_order(self):
         self.assertEqual(self._get_io_order_test(0),"[/] test oreder\n")
         self.assertEqual(self._get_io_order_test(1)," ├─ [/] test oreder\n")
         self.assertEqual(self._get_io_order_test(2)," │   ├─ [/] test oreder\n")
         self.assertEqual(self._get_io_order_test(3)," │       ├─ [/] test oreder\n")
         self.assertEqual(self._get_io_order_test(4)," │           ├─ [/] test oreder\n")
+    
+    def test_display_status_symbol_symbol(self):
+        self.assertEqual(self._get_io_symbol_test(0),"[/] test symbol\n")
+        self.assertEqual(self._get_io_symbol_test(1),"[x] test symbol\n")
+        self.assertEqual(self._get_io_symbol_test(2),"[*] test symbol\n")
+
+    def test_display_status_symbol_symbol_and_order(self):
+        self.assertEqual(self._get_io_order_symbol_test(0,0),"[/] test oreder and symbol\n")
+        self.assertEqual(self._get_io_order_symbol_test(1,1)," ├─ [x] test oreder and symbol\n")
+        self.assertEqual(self._get_io_order_symbol_test(2,2)," │   ├─ [*] test oreder and symbol\n")
+
         
 
 
