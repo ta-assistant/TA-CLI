0ab82b07761a8b069afea8014b06d2a04b7c01e1
vitvara <gridxlgm@gmail.com>
2021-07-29 22:17:15 +0700

    Update test make.py

--- test/test_make.py
+++ test/test_make.py
@@ -10,28 +10,32 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 class TestInitWorkDir(unittest.TestCase):
     def setUp(self) -> None:
         self.old_apikey_state = False
+        self.work_dir = os.path.join(currentdir,"work_dir")
+        os.mkdir(self.work_dir)
         return super().setUp()
         
     @patch('builtins.input',return_value='y')
-    def create_env(self,input):
+    def create_env(self,apikey,input):
         if exsitapikey():
             self.old_apikey_state = True
             self.old_apikey = readapikey()
-        self.assertIsNone(save("testKey"))
-        os.mkdir(os.path.join(currentdir,"work_dir"))
-        self.work_dir = os.path.join(currentdir,"work_dir")
-        print(readapikey())
+        self.assertIsNone(save(apikey))
 
     
     def test_init_work_dir(self):
-        self.create_env()
+        self.create_env("testKey")
         self.assertTrue(init_work_directory(self.work_dir,'testWork2'))
 
     def test_init_work_dir_invalid_workid(self):
-        self.create_env()
+        self.create_env("testKey")
         self.assertFalse(init_work_directory(self.work_dir,'testWork'))
-
-
+    
+    def test_init_work_dir_invalid_apikey(self):
+        self.create_env("testKey1")
+        self.assertFalse(init_work_directory(self.work_dir,'testWork2'))
+    
+    def test_init_work_dir_not_have_apikey(self):
+        self.assertFalse(init_work_directory(self.work_dir,'testWork2'))
 
     @patch('builtins.input',return_value='y')
     def tearDown(self,input) -> None:
