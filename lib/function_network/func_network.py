6bbc228695b4bcfd790874d2d8d12d50e5fcd568
Tanin Pewluangsawat <reiodean.dean@gmail.com>
2021-07-29 19:00:22 +0700

    add commend to func_network.py

--- lib/function_network/func_network.py
+++ lib/function_network/func_network.py
@@ -6,7 +6,17 @@ from lib.file_management.create_apikeyfile import *
 
 
 class Api:
+    """
+    Api
+    Create a base code that is required.
+    """
     def __init__(self, path) -> None:
+        """
+        Parameter
+        -------------------------------------------------------
+        path:
+            Directory of config.josn file
+        """
         self.path = path
         self.apikey = readapikey()
         self.data = readconfig(self.path)
@@ -28,13 +38,41 @@ class Api:
         self.posturl = self.prefix+self.postapi
 
     def api_massage(self):
+        """
+        Function for reading response from api
+        ===========================================
+
+        Return
+        --------------------------------------------
+            json object
+            All the response from api in type of json
+        """
         return self.res.json()
 
 class CallApi(Api):
+    """
+    CallApi
+    call api to get a draft.json
+    """
     def __init__(self, path) -> None:
+        """
+        Parameter
+        -------------------------------
+        path:
+            directory of TA-CLI
+        """
         super().__init__(path)
         
     def fetch(self):
+        """
+        Function of calling api for fetch draft from api
+        =================================================
+
+        Return
+        -------------------------------------------------
+            boolean:
+                if call api success will return True but fail will return False
+        """
         self.res = requests.get(self.url, headers=self.hparameter)
         if self.res.status_code == 200:
             massage = self.res.json()["message"]
@@ -46,6 +84,15 @@ class CallApi(Api):
             return False
             
     def createworkdraft(self):
+        """
+        Function of calling api for pull draft from api to local
+        ========================================================
+
+        Return
+        -------------------------------------------------------
+            boolean:
+                if call api success will return True but fail will return False
+        """
         self.res = requests.get(self.url, headers=self.hparameter)
         if self.res.status_code == 200:
             massage = self.res.json()["message"]
@@ -58,16 +105,38 @@ class CallApi(Api):
             return False
 
     def writejson(self, data) -> None:
+        """
+        Function for write from api to draft.json
+        """
         draft_path = os.path.join(self.path, 'ta', "draft.json")
         with open(draft_path, "w") as create:
             json.dump(data, create)
 
 class SendData(Api):
+    """
+    SendData
+    send data from work.josn to server
+    """
     def __init__(self, path) -> None:
+        """
+        Parameter
+        -------------------------------
+        path:
+            directory of TA-CLI
+        """
         super().__init__(path)
         self.getworkDraft()
 
     def getworkDraft(self):
+        """
+        Function for read data form work.json abnd send data to api
+        ================================================================
+
+        Return
+        ----------------------------------------------------------------
+            boolean:
+                if sending data success will return True but fail will return False
+        """
         work = WorkEditor.read_filework(self, self.path)
         self.res = requests.post(
             self.posturl, headers=self.hparameter, data=json.dumps(work))
