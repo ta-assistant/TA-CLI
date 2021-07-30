4f48a933cc8a4e26446757ceaedb08bbaa422fc4
vitvara <gridxlgm@gmail.com>
2021-07-30 12:57:54 +0700

    add test make.py

--- test/test_make.py
+++ test/test_make.py
@@ -12,9 +12,6 @@ class TestInitWorkDir(unittest.TestCase):
         self.old_apikey_state = False
         self.work_dir = os.path.join(currentdir,"work_dir")
         os.mkdir(self.work_dir)
-        self.sucess = os.path.exists(os.path.join(currentdir,"work_dir","ta")) and \
-        os.path.exists(os.path.join(currentdir,"work_dir","ta","config.json"))
-        self.cant_fetch_draft = os.path.exists(os.path.join(currentdir,"work_dir","ta","config.json"))
         return super().setUp()
         
     @patch('builtins.input',return_value='y')
@@ -24,24 +21,44 @@ class TestInitWorkDir(unittest.TestCase):
             self.old_apikey = readapikey()
         self.assertIsNone(save_api_key(apikey))
 
-    
+    def check_file(self):
+        # all success can create config and fetch draft
+        self.sucess = os.path.exists(os.path.join(currentdir,"work_dir","ta")) and \
+            os.path.exists(os.path.join(currentdir,"work_dir","ta","config.json")) and os.path.exists(os.path.join(currentdir,"work_dir","ta","draft.json"))
+        # cannot fetch draft
+        self.cant_fetch_draft = os.path.exists(os.path.join(currentdir,"work_dir","ta")) and \
+            os.path.exists(os.path.join(currentdir,"work_dir","ta","config.json")) and not os.path.exists(os.path.join(currentdir,"work_dir","ta","draft.json"))
+
+
     def test_init_work_dir(self):
+        # all good
         self.create_env("testKey")
         self.assertTrue(init_work_directory(self.work_dir,'testWork2'))
+        self.check_file()
         self.assertTrue(self.sucess)
-
+        self.assertFalse(self.cant_fetch_draft)
+        
     def test_init_work_dir_invalid_workid(self):
         self.create_env("testKey")
+        # wrong work id
         self.assertFalse(init_work_directory(self.work_dir,'testWork'))
+        self.check_file()
+        self.assertFalse(self.sucess)
         self.assertTrue(self.cant_fetch_draft)
 
     def test_init_work_dir_invalid_apikey(self):
+        # wrong api-key
         self.create_env("testKey1")
         self.assertFalse(init_work_directory(self.work_dir,'testWork2'))
+        self.check_file()
+        self.assertFalse(self.sucess)
         self.assertTrue(self.cant_fetch_draft)
 
     def test_init_work_dir_not_have_apikey(self):
+        # not have api-key
         self.assertFalse(init_work_directory(self.work_dir,'testWork2'))
+        self.check_file()
+        self.assertFalse(self.sucess)
         self.assertTrue(self.cant_fetch_draft)
 
     @patch('builtins.input',return_value='y')
